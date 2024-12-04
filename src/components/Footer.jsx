792c5c63244448024bc1378b93fc337011c8788a
dwiPriyatmoko <dwipriyatmoko27@gmail.com>
2024-12-04 18:25:08 +0700

    update accent colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ const socialLinks = [
 
 const Footer = () => {
 	return (
-		<footer className="w-screen bg-[#7a7a7a] py-4 text-black">
+		<footer className="w-screen bg-[#c6c6c6] py-4 text-black">
 			<div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
 				<p className="text-center text-sm font-light md:text-left">
 					©Zanimate 2024. All rights reserved
